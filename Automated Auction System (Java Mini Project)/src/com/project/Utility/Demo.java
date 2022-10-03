543087653a0fd4516439b7c89f43ef6bd8e27cfa
samrat <smrtsnh1995@gmail.com>
2022-10-03 20:29:41 +0530

    Day-1 JDBC Connection Created

---
+++ Automated Auction System (Java Mini Project)/src/com/project/Utility/Demo.java
@@ -0,0 +1,13 @@
+package com.project.Utility;
+
+import java.sql.Connection;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Connection conn = DBUtil.provideConnection();
+		System.out.println(conn);
+		
+	}
+}
