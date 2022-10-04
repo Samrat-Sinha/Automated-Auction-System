e78f24c9260873837af637836700d23766d33b28
samrat <smrtsnh1995@gmail.com>
2022-10-04 22:45:11 +0530

    Project Day-2

---
+++ Automated Auction System (Java Mini Project)/src/com/project/frontend/App.java
@@ -0,0 +1,11 @@
+package com.project.frontend;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" - - - - - - - - - - - - - - Welcome to Automated Auction System - - - - - - - - - - - - - - ");
+        auctionApp.selectoption();
+	}
+
+}
